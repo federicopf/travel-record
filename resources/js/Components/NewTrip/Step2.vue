8393105b66e4d60261b4e0128c50c7ecddec85ec
follia <f.germani06@gmail.com>
2025-02-23 03:35:30 +0100

    [WIP] Integrato maps

--- resources/js/Components/NewTrip/Step2.vue
+++ resources/js/Components/NewTrip/Step2.vue
@@ -1,34 +1,54 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const props = defineProps(['modelValue']);
 const emit = defineEmits(['update:modelValue']);
-const newPlace = ref('');
 
-const addPlace = () => {
-    if (newPlace.value) {
-        props.modelValue.places.push({ name: newPlace.value, photos: [] });
-        newPlace.value = '';
-        emit('update:modelValue', props.modelValue);
+const newPlace = ref('');
+const placeInput = ref(null);
+
+onMounted(() => {
+    if (window.google) {
+        const autocomplete = new window.google.maps.places.Autocomplete(placeInput.value, {
+            types: ['geocode'], // Solo indirizzi
+            componentRestrictions: { country: 'IT' }, // Opzionale: Limita a un paese
+        });
+
+        autocomplete.addListener('place_changed', () => {
+            const place = autocomplete.getPlace();
+            if (place.geometry) {
+                props.modelValue.places.push({
+                    name: place.name,
+                    address: place.formatted_address,
+                    lat: place.geometry.location.lat(),
+                    lng: place.geometry.location.lng(),
+                    photos: []
+                });
+
+                emit('update:modelValue', props.modelValue);
+                newPlace.value = ''; // Resetta l'input
+            }
+        });
     }
-};
+});
 </script>
 
 <template>
     <div>
         <label class="block mb-2 text-gray-700">Aggiungi un posto da visitare</label>
+
         <div class="flex gap-2">
-            <input v-model="newPlace" type="text" placeholder="Es. Shibuya Crossing"
+            <input ref="placeInput" v-model="newPlace" placeholder="Es. Colosseo, Roma"
                 class="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-pink-300">
-            <button @click="addPlace"
-                class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition">
+
+            <button class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition">
                 +
             </button>
         </div>
 
         <ul class="mt-4">
             <li v-for="(place, index) in modelValue.places" :key="index" class="p-2 border-b text-gray-700">
-                {{ place.name }}
+                <strong>{{ place.name }}</strong> ({{ place.lat }}, {{ place.lng }})
             </li>
         </ul>
     </div>
--- resources/js/app.js
+++ resources/js/app.js
@@ -13,3 +13,15 @@ createInertiaApp({
             .mount(el);
     },
 });
+
+const loadGoogleMapsScript = () => {
+    if (!window.google) {
+        const script = document.createElement("script");
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places`;
+        script.async = true;
+        script.defer = true;
+        document.head.appendChild(script);
+    }
+};
+
+loadGoogleMapsScript();
