e97057a63f95825f9e730a4da8739c331e72e058
follia <f.germani06@gmail.com>
2025-03-05 18:19:47 +0100

    [REVERT]

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -52,6 +52,7 @@ class TripController extends Controller
             'places.*.name' => 'required|string|max:255',
             'places.*.lat' => 'required|numeric',
             'places.*.lng' => 'required|numeric',
+            'places.*.photos' => 'array|max:30', 
             'places.*.photos.*' => 'nullable|file|mimes:jpeg,png,jpg,webp,mp4,mov,avi|max:5120',
             'favorite_photo' => 'nullable|string',
         ]);
