5d85003797cd8b1ba3b8e6e1bf6251cc13134060
follia <f.germani06@gmail.com>
2025-04-08 17:57:52 +0200

    [WIP] Pulsante back su preview profilo

--- resources/js/Pages/Profile/Public/Index.vue
+++ resources/js/Pages/Profile/Public/Index.vue
@@ -19,6 +19,16 @@ const isMe = computed(() => usePage().props.auth?.user?.id === props.user?.id)
 <template>
   <AppLayout>
     <div class="max-w-3xl mx-auto px-6 py-8">
+
+      <div v-if="user.preview" class="mb-4">
+        <a
+          :href="route('profile.index', user.id)"
+          class="inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+        >
+          Torna alla gestione profilo
+        </a>
+      </div>
+
       <div class="mb-6">
         <h1 class="text-3xl font-bold text-gray-800">
           Profilo di {{ user.name }}
